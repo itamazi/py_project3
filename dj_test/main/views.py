fc73185605b6c4bca28ffc7ca6993a2d450983ca
itamazi <t.ismagulov@gmail.com>
2023-09-13 13:00:08 +0600

    Add1

---
+++ dj_test/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+# def about(request):
+#     return HttpResponse('<h4>Информация о нас</h4>')
