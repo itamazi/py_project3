6e58062964e6725094ccce16ec3b459c6a1ca5eb
itamazi <t.ismagulov@gmail.com>
2023-09-17 18:05:40 +0600

    Add3

--- dj_test/main/views.py
+++ dj_test/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title': 'Основная страница'})
 
 def about(request):
     return render(request, 'main/about.html')
