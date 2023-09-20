830b8cd929cd2ca18cbfd25b5ba2c33f05e0858e
itamazi <t.ismagulov@gmail.com>
2023-09-20 19:07:15 +0600

    Add5

--- dj_test/news/views.py
+++ dj_test/news/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
+
+
 def news_home(request):
     news = Articles.objects.order_by('-date') # здесь мы импортируем и сортируем статьи по дате
     #news = Articles.objects.all() # здесь мы из класса articles импортируем все объекты (статьи)
     return render(request, 'news/news_home.html', {'news': news})
+
+# создаем класс NewsDetailView для открытия динамических страниц со статьями
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/news-delete.html'
 
 def create(request):
     error = ''
